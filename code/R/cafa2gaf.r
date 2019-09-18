8c077eb70e8b4eeda06526e5ff0cd8f20e0024ea
Kokulapalan Wimalanathan <kokul@iastate.edu>
2019-09-17 19:21:54 -0500

    updated fix to search for pannzer results

--- code/R/cafa2gaf.r
+++ code/R/cafa2gaf.r
@@ -23,7 +23,7 @@ argot2_gaf=paste(gaf_dir,paste(config$input$species,config$input$inbred,config$i
 filter_argot2(in_file=argot2_results,out_file=argot2_gaf,config=config)
 
 #processing PANNZER results
-pannzer_results =  dir(config$`mixed-meth`$PANNZER$result_dir,pattern = ".GO",full.names = T)
+pannzer_results =  dir(config$`mixed-meth`$PANNZER$result_dir,pattern = "_results.GO$",full.names = T)
 pannzer_gaf = paste(gaf_dir,paste(config$input$species,config$input$inbred,config$input$version,"pannzer","gaf",sep="."),sep = "")
 pannzer2gaf(in_files = pannzer_results,out_gaf=pannzer_gaf,config)
 
